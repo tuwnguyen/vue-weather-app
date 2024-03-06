74dd3d7145fbf155b072ea40d89eeaa98f99840a
tuwnguyen <tuwnguyen@gmail.com>
2024-03-06 17:54:06 +0700

    Handling Geocoding api errors/no data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,20 @@ const mapboxAPIKey = 'pk.eyJ1IjoidHV3bmd1eWVuIiwiYSI6ImNsdGZtdjdhMDByd24yam8zbXM
 const searchQuery = ref('')
 const queryTimeout = ref(null)
 const mapboxSearchResults = ref(null)
+const searchError = ref(null)
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout);
   queryTimeout.value = setTimeout(async () => {
-    const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`)
-    mapboxSearchResults.value = result.data.features
-    console.log(':::result', mapboxSearchResults.value)
+    if (searchQuery !== "") {
+      try {
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`)
+        mapboxSearchResults.value = result.data.features
+      } catch (error) {
+        searchError.value = true
+      }
+      return
+    }
   }, 300)
   mapboxSearchResults.value = null
 }
@@ -31,15 +38,23 @@ const getSearchResults = () => {
         class="py-2 px-1 w-full bg-transparent border-b 
         focus:border-weather-secondary focus:outline-none focus:shadow=[0px_1px_0_0#004E71]"
       />
-      <ul 
+      <ul
         v-if="mapboxSearchResults"
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]">
-        <li 
-          v-for="searchResult in mapboxSearchResults" :key="searchResult.id"
-          class="py-2 cursor-pointer"
-        >
-          {{ searchResult.place_name }}
-        </li>
+        <p v-if="searchError" class="py-2">
+          Sorry, something went wrong, please try again
+        </p>
+        <p v-if="!searchError && mapboxSearchResults.length === 0" class="py-2">
+          No results match your query, trye a different term
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in mapboxSearchResults" :key="searchResult.id"
+            class="py-2 cursor-pointer"
+          >
+            {{ searchResult.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
