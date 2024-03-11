074fd2b69c069a60507f294b55dd1ee510d211a2
tuwnguyen <tuwnguyen@gmail.com>
2024-03-12 00:30:12 +0700

    City view layout: imp list hourly weather, daily weather in 7 days

--- src/views/AsyncCityView.vue
+++ src/views/AsyncCityView.vue
@@ -43,7 +43,72 @@
       </p>
       <img 
         class="w-[150px] h-auto"
-        :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`" alt="">
+        :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`" alt=""
+      >
+    </div>
+    
+    <hr class="border-white border-opacity-10 border w-full"/>
+    
+    <!-- Hourly Weather -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8  text-white ">
+        <h2 class="mb-4"> Hourly Weather</h2>
+        <div class="flex gap-10 overflow-x-scroll">
+          <div
+            class="flex flex-col gap-4 items-center"
+            v-for="hourData in weatherData.hourly"
+            :key="hourData.dt"
+          >
+            <p class="whitespace-nowrap text-md">
+              {{ 
+                new Date(hourData.currentTime).toLocaleTimeString(
+                  "en-us",
+                  {hour: "numeric"}
+                )
+              }}
+            </p>
+            <img 
+              class="w-auto h-[50px] object-cover"
+              :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
+              alt=""
+            >
+            <p class="text-xl">
+              {{ Math.round(hourData.temp) }}&deg;
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <hr class="border-white border-opacity-10 border w-full"/>
+    
+    <!-- Weekly Weather -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8  text-white ">
+        <h2 class="mb-4"> 7-Day Forecast</h2>
+          <div
+            class="flex items-center"
+            v-for="day in weatherData.daily"
+            :key="day.dt"
+          >
+            <p class="flex-1">
+              {{ 
+                new Date(day.dt * 1000).toLocaleDateString(
+                  "en-us",
+                  {weekday: "long"}
+                )
+              }}
+            </p>
+            <img 
+              class="w-[50px] h-[50px] object-cover"
+              :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+            >
+            <div class="flex gap-2 flex-1 justify-end">
+              <p>H: {{ Math.round(day.temp.max) }}&deg;</p>
+              <p>L: {{ Math.round(day.temp.min) }}&deg;</p>
+            </div>
+          </div>
+      </div>
     </div>
   </div>
 </template>
