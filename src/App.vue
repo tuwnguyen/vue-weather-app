cb68e06976712b30fbbb223b58c6213cd68fc291
tuwnguyen <tuwnguyen@gmail.com>
2024-03-12 22:18:03 +0700

    Route Transition: imp transition for routerView, edit the metadata title of each page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation />
-    <RouterView></RouterView>
+    <RouterView v-slot="{ Component }">
+      <Transition name="page" mode="out-in">
+        <component :is="Component"></component>
+      </Transition>
+    </RouterView>
   </div>
 </template>
 <script setup>
   import { RouterView } from 'vue-router';
   import SiteNavigation from './components/SiteNavigation.vue'
 </script>
-<style lang="scss" scoped>
-  
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: 600ms ease all;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
 </style>
